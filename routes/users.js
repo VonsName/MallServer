08f31c7772ceb814f8018a135ed58586944317e2
VonsName <894611653@qq.com>
2018-07-05 23:12:39 +0800

    14 11-3

--- routes/users.js
+++ routes/users.js
@@ -22,6 +22,10 @@ router.post("/login",(req,res,next)=>{
             path:"/",
             maxAge:1000*60*60
           });
+          res.cookie("userName",doc.userName,{
+              path:"/",
+              maxAge:1000*60*60
+          });
           //res.session.user=doc;
           res.json({
               "Status":"1",
@@ -47,9 +51,75 @@ router.get("/logout",(req,res)=>{
     maxAge:-1
   });
   res.json({
-     "Status":"1",
+     "Status":"0",
      "msg":"",
      "result":"fail"
   });
+});
+
+router.get('/checkLogin',(req,res,next)=>{
+    if (req.cookies.userId){
+        res.json({
+            "Status":"1",
+            "msg":"success",
+            "result":req.cookies.userName
+        })
+    }else {
+        res.json({
+            "Status":"0",
+            "msg":"fail 未登录",
+            "result":""
+        })
+    }
+});
+
+//查询我的购物车
+router.get("/cartList",(req,res,next)=>{
+    let userId = req.cookies.userId;
+   Users.findOne({userId:userId},(err,doc)=>{
+       if (err){
+           res.json({
+               "Status":"0",
+               "msg":"",
+               "result":"fail"
+           });
+       } else {
+           if (doc){
+               res.json({
+                   "Status":"1",
+                   "msg":"success",
+                   "result":doc.cartList
+               });
+           } else {
+               res.json({
+                   "Status":"1",
+                   "msg":"success",
+                   "result":"您的购物车还没有数据,前往商品列表购物吧!"
+               });
+           }
+       }
+   })
+});
+//删除购物车商品
+router.post("/cart/del",(req,res,next)=>{
+   let userId = req.cookies.userId;
+   let productId = req.body.productId;
+   console.log(productId);
+   Users.update({userId:userId},{$pull:{cartList:{productId:productId}}},(err,data)=>{
+        if (err){
+            res.json({
+                "Status":"0",
+                "msg":"fail",
+                "result":"删除失败"
+            });
+        } else {
+            console.log(data);
+            res.json({
+                "Status":"1",
+                "msg":"success",
+                "result":"删除成功"
+            });
+        }
+   })
 });
 module.exports = router;
